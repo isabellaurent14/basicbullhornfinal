1abbc7925e4f19dbd8684904bae82c9281be5e68
Isabel Navarro-Laurent <isabelnavarro-laurent@Isabels-MacBook-Air.local>
2018-12-07 19:21:27 -0500

    Initial commit

---
+++ src/main/java/com/example/basicbullhornfinal/BasicbullhornfinalApplication.java
@@ -0,0 +1,12 @@
+package com.example.basicbullhornfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicbullhornfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicbullhornfinalApplication.class, args);
+    }
+}
